e54fe4e334fffcc60ac1ecee5103f442c71bdbc4
JonW08 <Jonweider07@gmail.com>
2025-02-10 14:16:32 +0100

    testing

---
+++ scripts/info.js
@@ -0,0 +1,47 @@
+
+const parsedUrl = new URL(window.location.href);
+const produktnavn = parsedUrl.searchParams.get("produkt");
+const bilder = parsedUrl.searchParams.get("bilde");
+
+async function fetchProducts() {
+    try {
+        const response = await fetch('scripts/produkt.json');
+        if (!response.ok) {
+            throw new Error('Kunne ikke laste produkter.json');
+        }
+        const products = await response.json();
+        return products;
+    } catch (error) {
+        console.error('Feil ved henting av produkter:', error);
+    }
+}
+
+function generer(products) {
+    products.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('card');
+        productCard.value = product.name;
+        productCard.id = product.name;
+        
+    });
+
+    function getProduct() {
+        const KvaProduct = products.find(item => item.name === produktnavn);
+        return KvaProduct || { name: "No Fysikk product found." };
+    }
+
+    const KvaProduct = getProduct();
+    document.getElementById("title").innerHTML = KvaProduct.name;
+    document.getElementById("pris").innerHTML = KvaProduct.price+"kr";
+    
+    document.getElementById("BildeId").src = KvaProduct.image;
+    document.getElementById("merinfo").innerHTML = KvaProduct.moredescription;
+    
+    console.log(fysikkProduct);
+}
+
+fetchProducts().then(products => {
+    if (products) {
+        generer(products);
+    }
+});
---
+++ scripts/leggIHandlekurv.js
@@ -0,0 +1,27 @@
+function LeggIKurven() {
+    
+        const parsedUrl = new URL(window.location.href);
+        const produktnavn = parsedUrl.searchParams.get("produkt");
+        const pris = parsedUrl.searchParams.get("pris");
+        
+    
+        // Hent eksisterende handlekurv fra localStorage eller opprett en ny
+        let cart;
+        try {
+            cart = JSON.parse(localStorage.getItem('cart')) || [];
+        } catch (error) {
+            console.error("Feil ved parsing av handlekurv:", error);
+            cart = []; // Hvis parsing feiler, start med en tom liste
+        }
+
+        // Legg til produktet i handlekurven
+        cart.push({ produktnavn, pris });
+      
+        // Lagre den oppdaterte handlekurven i localStorage
+        localStorage.setItem('cart', JSON.stringify(cart));
+      
+        alert(`${produktnavn} er lagt i handlekurven!`);
+      
+      
+
+}
